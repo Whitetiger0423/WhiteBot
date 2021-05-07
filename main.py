f5650d9ecded41d05ff218c4949b6fed0e5192f0
Whitetiger <72282371+Whitetiger0423@users.noreply.github.com>
2021-05-07 15:11:27 +0900

    set token

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ except ImportError:
 
 
 bot = commands.Bot(command_prefix='/', help_command=None)
-DBKR_token = os.getenv('DBKR_token')
+DBKR_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc4Mjc3NzAzNTg5ODYxNzg4NiIsImlhdCI6MTYxNjU5ODI3NSwiZXhwIjoxNjQ4MTU1ODc1fQ.d5e3kEoj3NtTjM56awSfTQHmcFdtwf9fj4NdAPUF2YAqjlJQPsDTqOzkbX0_HMI9OcOQOvPQNm3JOP18IMth9qQmy0Pzhx__JwFoGd5oQEUnqJe54y0utr7vRqstRJ0zlaUHbfkb8IR6CD5T-zieLvq_Cv4q_XmCxaHCn4GiScg'
 
 
 dab = db["dab"]
@@ -103,4 +103,4 @@ async def announce(ctx, *, 내용):
     else:
         await ctx.send('권한이 부족합니다.')
 
-bot.run(os.getenv("TOKEN"))
+bot.run("NzgyNzc3MDM1ODk4NjE3ODg2.X8RH7A.IggBBwjsTf17zD3amlo5GhMopmg")
