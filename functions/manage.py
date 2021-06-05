cdc9bfda19a8cf17cb95f622db2f5e081e417f56
Whitetiger <72282371+Whitetiger0423@users.noreply.github.com>
2021-06-05 12:59:54 +0900

    update for commands.command

--- functions/manage.py
+++ functions/manage.py
@@ -18,109 +18,109 @@ class manage(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@commands.command()
-async def 핑(ctx):
-    embed = discord.Embed(title=':ping_pong: 퐁!', color=0xFEFEFE)
-    embed.set_thumbnail(url=bot.user.avatar_url)
-    embed.add_field(name='Discord API Ping: ',
+    @commands.command()
+    async def 핑(ctx):
+        embed = discord.Embed(title=':ping_pong: 퐁!', color=0xFEFEFE)
+        embed.set_thumbnail(url=bot.user.avatar_url)
+        embed.add_field(name='Discord API Ping: ',
                     value=f'{round(bot.latency * 1000)} ms')
-    await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
 
-@commands.command()
-async def 청소(ctx, count: int):
-    if ctx.author.guild_permissions.administrator:
-        await asyncio.sleep(2)
-        await ctx.channel.purge(limit=count + 1)
-        erasemsg = await ctx.send(f'`{count}`건의 메시지를 청소했습니다.')
-        await asyncio.sleep(3)
-        await erasemsg.delete()
-    else:
-        await ctx.send(
-            f'{ctx.author.mention} 메시지 관리 권한이 필요합니다. 권한 확인 후 다시 실행해주세요.')
-
-
-@commands.command()
-async def 킥(ctx, member: discord.Member, *, reason):
-    if ctx.author.guild_permissions.administrator:
-        if not member.guild_permissions.administrator:
-            await ctx.send(
-                f'{member.name}#{member.discriminator} 서버원을 추방하였습니다.')
-            await member.send(
-                f"{ctx.message.guild.name} 서버에서 추방당했습니다! 이유는 다음과 같습니다: ```{reason}``` 서버에 다시 참가할 수 있습니다! \n {await ctx.message.guild.invites()[0]}"
-            )
-            await member.kick(reason=reason)
+    @commands.command()
+    async def 청소(ctx, count: int):
+        if ctx.author.guild_permissions.administrator:
+            await asyncio.sleep(2)
+            await ctx.channel.purge(limit=count + 1)
+            erasemsg = await ctx.send(f'`{count}`건의 메시지를 청소했습니다.')
+            await asyncio.sleep(3)
+            await erasemsg.delete()
         else:
             await ctx.send(
-                f'{ctx.author.mention} 관리자는 추방할 수 없습니다. 관리자 권한을 해제하고 다시 시도해주세요.'
-            )
-    else:
-        await ctx.send(
-            f'{ctx.author.mention} 관리자 권한이 필요합니다. 권한 확인 후 다시 실행해주세요.')
+                f'{ctx.author.mention} 메시지 관리 권한이 필요합니다. 권한 확인 후 다시 실행해주세요.')
 
 
-@commands.command()
-async def 밴(ctx, member: discord.Member, *, reason):
-    if ctx.author.guild_permissions.administrator:
-        if not member.guild_permissions.administrator:
-            await ctx.send(f'{member.name}#{member.discriminator} 서버원을 밴했습니다.')
-            await member.send(
+    @commands.command()
+    async def 킥(ctx, member: discord.Member, *, reason):
+        if ctx.author.guild_permissions.administrator:
+            if not member.guild_permissions.administrator:
+                await ctx.send(
+                    f'{member.name}#{member.discriminator} 서버원을 추방하였습니다.')
+                await member.send(
+                    f"{ctx.message.guild.name} 서버에서 추방당했습니다! 이유는 다음과 같습니다: ```{reason}``` 서버에 다시 참가할 수 있습니다! \n {await ctx.message.guild.invites()[0]}"
+                )
+                await member.kick(reason=reason)
+            else:
+                await ctx.send(
+                    f'{ctx.author.mention} 관리자는 추방할 수 없습니다. 관리자 권한을 해제하고 다시 시도해주세요.'
+                )
+        else:
+            await ctx.send(
+                f'{ctx.author.mention} 관리자 권한이 필요합니다. 권한 확인 후 다시 실행해주세요.')
+
+
+    @commands.command()
+    async def 밴(ctx, member: discord.Member, *, reason):
+        if ctx.author.guild_permissions.administrator:
+            if not member.guild_permissions.administrator:
+                await ctx.send(f'{member.name}#{member.discriminator} 서버원을 밴했습니다.')
+                await member.send(
                 f"{ctx.message.guild.name} 서버에서 차단당했습니다! 이유는 다음과 같습니다: ```{reason}``` 차단이 풀리기 전까지 서버에 다시 참가할 수 없습니다!"
             )
-            await member.ban(reason=reason)
+                await member.ban(reason=reason)
+            else:
+                await ctx.send(
+                    f'{ctx.author.mention} 관리자는 밴할 수 없습니다. 관리자 권한을 해제하고 다시 시도해주세요.'
+            )
         else:
             await ctx.send(
-                f'{ctx.author.mention} 관리자는 밴할 수 없습니다. 관리자 권한을 해제하고 다시 시도해주세요.'
-            )
-    else:
-        await ctx.send(
-            f'{ctx.author.mention} 관리자 권한이 필요합니다. 권한 확인 후 다시 실행해주세요.')
+                f'{ctx.author.mention} 관리자 권한이 필요합니다. 권한 확인 후 다시 실행해주세요.')
 
 
-@commands.command()
-async def 언밴(ctx, *, member):
-    if ctx.author.guild_permissions.administrator:
-        banned_users = await ctx.guild.bans()
-        member_name, member_discriminator = member.split('#')
-        for ban_entry in banned_users:
-            user = ban_entry.user
-            if (user.name, user.discriminator) == (member_name,
+    @commands.command()
+    async def 언밴(ctx, *, member):
+        if ctx.author.guild_permissions.administrator:
+            banned_users = await ctx.guild.bans()
+            member_name, member_discriminator = member.split('#')
+            for ban_entry in banned_users:
+                user = ban_entry.user
+                if (user.name, user.discriminator) == (member_name,
                                                    member_discriminator):
-                await ctx.send('서버원을 언밴했습니다. 이 서버에 다시 들어올 수 있습니다.')
-                await ctx.guild.unban(user)
-    else:
-        await ctx.send(
-            f'{ctx.author.mention} 관리자 권한이 필요합니다. 권한 확인 후 다시 실행해주세요.')
+                    await ctx.send('서버원을 언밴했습니다. 이 서버에 다시 들어올 수 있습니다.')
+                    await ctx.guild.unban(user)
+        else:
+            await ctx.send(
+                f'{ctx.author.mention} 관리자 권한이 필요합니다. 권한 확인 후 다시 실행해주세요.')
 
-@commands.command()
-async def 서버(ctx):
-    embed = discord.Embed(title='서버 정보', color=0xFEFEFE)
-    embed.set_thumbnail(url=ctx.guild.icon_url)
-    embed.add_field(name='서버 이름: ',
+    @commands.command()
+    async def 서버(ctx):
+        embed = discord.Embed(title='서버 정보', color=0xFEFEFE)
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        embed.add_field(name='서버 이름: ',
                     value=f'{ctx.guild.name}', inline = False)
-    embed.add_field(name='서버 ID: ',
+        embed.add_field(name='서버 ID: ',
                     value=f'{ctx.guild.id}', inline = False)
-    embed.add_field(name='서버 지역: ',
+        embed.add_field(name='서버 지역: ',
                     value=f'{ctx.guild.region}', inline = False)
-    embed.add_field(name='서버 부스트 레벨: ',
+        embed.add_field(name='서버 부스트 레벨: ',
                     value=f'{ctx.guild.premium_tier}레벨', inline = False)
-    embed.add_field(name='멤버 수: ',
+        embed.add_field(name='멤버 수: ',
                     value=f'{ctx.guild.member_count}', inline = False)
-    await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
-@commands.command()
-async def 내정보(ctx):
-    embed = discord.Embed(title='유저 정보', color=0xFEFEFE)
-    embed.set_thumbnail(url=ctx.author.avatar_url)
-    embed.add_field(name='계정명: ',
+    @commands.command()
+    async def 내정보(ctx):
+        embed = discord.Embed(title='유저 정보', color=0xFEFEFE)
+        embed.set_thumbnail(url=ctx.author.avatar_url)
+        embed.add_field(name='계정명: ',
                     value=f'{ctx.author.name}', inline = False)
-    embed.add_field(name='ID: ',
+        embed.add_field(name='ID: ',
                     value=f'{ctx.author.id}', inline = False)
-    embed.add_field(name='이 서버에서의 별명: ',
+        embed.add_field(name='이 서버에서의 별명: ',
                     value=f'{ctx.author.display_name}', inline = False)
-    embed.add_field(name='멘션: ',
+        embed.add_field(name='멘션: ',
                     value=f'{ctx.author.mention}', inline = False)
-    await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(manage(bot))
--- functions/playing.py
+++ functions/playing.py
@@ -18,32 +18,32 @@ class playing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@commands.command()
-async def 가위바위보(ctx, user: str):
-    rps_table = ['가위', '바위', '보']
-    bot = random.choice(rps_table)
-    result = rps_table.index(user) - rps_table.index(bot)
-    if result == 0:
-        await ctx.send(
-            f'{user} vs {bot}\n{ctx.author.mention} 비겼네요! 다시 한판 하는건 어때요?')
-    elif result == 1 or result == -2:
-        await ctx.send(
-            f'{user} vs {bot}\n{ctx.author.mention}님이 이겼어요! 절 이기시다니 대단해요!'
-        )
-    else:
-        await ctx.send(
-            f'{user} vs {bot}\n{ctx.author.mention} 제가 이겼습니다! 한판 더 하실래요?')
+    @commands.command()
+    async def 가위바위보(ctx, user: str):
+        rps_table = ['가위', '바위', '보']
+        bot = random.choice(rps_table)
+        result = rps_table.index(user) - rps_table.index(bot)
+        if result == 0:
+            await ctx.send(
+                f'{user} vs {bot}\n{ctx.author.mention} 비겼네요! 다시 한판 하는건 어때요?')
+        elif result == 1 or result == -2:
+            await ctx.send(
+                f'{user} vs {bot}\n{ctx.author.mention}님이 이겼어요! 절 이기시다니 대단해요!'
+            )
+        else:
+            await ctx.send(
+                f'{user} vs {bot}\n{ctx.author.mention} 제가 이겼습니다! 한판 더 하실래요?')
 
-@commands.command()
-async def 주사위(ctx, 첫번째숫자: int, 두번째숫자: int = None):
-    if (두번째숫자):
-        await ctx.send(
-            f"{ctx.author.mention} 주사위를 굴렸더니 {random.randint(첫번째숫자, 두번째숫자)}(이)가 나왔어요!"
-        )
-    else:
-        await ctx.send(
-            f"{ctx.author.mention} 주사위를 굴렸더니 {random.randint(1, 첫번째숫자)}(이)가 나왔어요!"
-        )
+    @commands.command()
+    async def 주사위(ctx, 첫번째숫자: int, 두번째숫자: int = None):
+        if (두번째숫자):
+            await ctx.send(
+                f"{ctx.author.mention} 주사위를 굴렸더니 {random.randint(첫번째숫자, 두번째숫자)}(이)가 나왔어요!"
+            )
+        else:
+            await ctx.send(
+                f"{ctx.author.mention} 주사위를 굴렸더니 {random.randint(1, 첫번째숫자)}(이)가 나왔어요!"
+            )
 
 def setup(bot):
     bot.add_cog(playing(bot))
