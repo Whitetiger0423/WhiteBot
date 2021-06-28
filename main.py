ea337b440014e6648fc9914b4b17dabffd355f49
주식마스터 <79557069+jaewoolee82@users.noreply.github.com>
2021-06-28 21:17:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,36 +23,7 @@ async def on_ready():
     print("다음으로 로그인합니다 : ")
     print(bot.user.name)
 
-@bot.command(name="유튜브")
-async def youtu1be(ctx): 
-    red = 0xFF0000
-    blue = 0x00FFFF
-    voice = ctx.author.voice
 
-    if not voice:
-        embed = discord.Embed(description="이 명령을 사용하려면 사용자가 음성 채널에 있어야합니다...", color=red)
-        return await ctx.channel.send(embed=embed)
-
-    r = Route("POST", "/channels/{channel_id}/invites", channel_id=voice.channel.id)
-
-    payload = {
-        "max_age": 0,
-        "target_type": 2,
-        "target_application_id": 755600276941176913,
-    }
-
-    try:
-        code = (await self.bot.http.request(r, json=payload))["code"]
-    except discord.Forbidden:
-            embed = discord.Embed(description="봇이 초대할 수 있는 권한이 없습니다...")
-            return await ctx.channel.send(embed=embed)
-
-    embed = discord.Embed(title="유튜브 투게더",
-            description=f"[여기를 클릭해주세요](https://discord.gg/{code})",
-            color=blue,
-        )
-    await ctx.send(embed=embed)
-    
     
 for filename in os.listdir("functions"):
     if filename.endswith(".py"):
