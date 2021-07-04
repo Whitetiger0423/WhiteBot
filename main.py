4babcc6bd311d35d5d4d9eb449fc449219aa62f3
주식마스터 <79557069+jaewoolee82@users.noreply.github.com>
2021-07-04 17:06:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ try:
 except ImportError:
     os.system("pip install dbkrpy")
     import dbkrpy
+try:
+    import koreanbots
+except ImportError:
+    os.system("pip install koreanbots")
+    import koreanbots
 from functions import *
 
 
 bot = commands.Bot(command_prefix='/', help_command=None)
 DBKR_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc4Mjc3NzAzNTg5ODYxNzg4NiIsImlhdCI6MTYxNjU5ODI3NSwiZXhwIjoxNjQ4MTU1ODc1fQ.d5e3kEoj3NtTjM56awSfTQHmcFdtwf9fj4NdAPUF2YAqjlJQPsDTqOzkbX0_HMI9OcOQOvPQNm3JOP18IMth9qQmy0Pzhx__JwFoGd5oQEUnqJe54y0utr7vRqstRJ0zlaUHbfkb8IR6CD5T-zieLvq_Cv4q_XmCxaHCn4GiScg'
-
-
-dbkrpy.UpdateGuilds(bot,DBKR_token)
+Bot = koreanbots.Client(client, DBKR_token)
 
 @bot.event
 async def on_ready():
