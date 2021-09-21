df07f7ff4c727139e73cab1499b2fa8cc74fded9
Whitetiger <teowhite0423@gmail.com>
2021-09-21 05:27:59 +0000

    주사위 명령어 개선, 가위바위보 명령어 일부 수정

--- functions/playing.py
+++ functions/playing.py
@@ -20,7 +20,7 @@ class playing(commands.Cog):
     async def 가위바위보(self, ctx, user: str):
         rps_table = ['가위', '바위', '보']
         if user not in rps_table:
-            await ctx.send('가위, 바위, 보 중에 하나를 입력해주세요.')
+            await ctx.send('가위/바위/보 중에 하나를 입력해주세요.')
         else:
             bot = random.choice(rps_table)
             result = rps_table.index(user) - rps_table.index(bot)
@@ -37,17 +37,20 @@ class playing(commands.Cog):
 
     @commands.command()
     async def 주사위(self, ctx, 첫번째숫자: int, 두번째숫자: int = None):
-        if 첫번째숫자 < 1:
-            await ctx.send('자연수 값만 허용됩니다.')
-        else:
-            if (두번째숫자):
-                await ctx.send(
-                    f"{ctx.author.mention} 주사위를 굴렸더니 {random.randint(첫번째숫자, 두번째숫자)}(이)가 나왔어요!"
-                )
+        try:
+            if 첫번째숫자 < 1:
+                await ctx.send('자연수 값만 허용됩니다.')
             else:
-                await ctx.send(
-                    f"{ctx.author.mention} 주사위를 굴렸더니 {random.randint(1, 첫번째숫자)}(이)가 나왔어요!"
-                )
+                if (두번째숫자):
+                    await ctx.send(
+                        f"{ctx.author.mention} 주사위를 굴렸더니 {random.randint(첫번째숫자, 두번째숫자)}(이)가 나왔어요!"
+                    )
+                else:
+                    await ctx.send(
+                        f"{ctx.author.mention} 주사위를 굴렸더니 {random.randint(1, 첫번째숫자)}(이)가 나왔어요!"
+                    )
+        except:
+            await ctx.send("오류 발생: 올바른 값을 넣어주세요.")
 
 def setup(bot):
     bot.add_cog(playing(bot))
