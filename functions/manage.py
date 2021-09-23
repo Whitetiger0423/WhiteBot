9e1f68fa56b12bacf9a68c8db88914c678ce84e7
Whitetiger <teowhite0423@gmail.com>
2021-09-23 13:03:56 +0900

    공지 명령어에서 엠베드 구문 인수 오류 수정

--- functions/manage.py
+++ functions/manage.py
@@ -143,11 +143,9 @@ class manage(commands.Cog):
         if ctx.author.id in adminid:
             now = datetime.datetime.now()
             time = f"{str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {str(now.hour)}시 {str(now.minute)}분 {str(now.second)}초"
-            embed = nextcord.Embed(color=0x00FFFF)
             embed = nextcord.Embed(title=":loudspeaker: WhiteBot 공지",
                               description=" ",
-                              color=0xffffff,
-                              inline=False)
+                              color=0xffffff)
             embed.add_field(name='공지 내용', value=f'{내용}')
             embed.set_footer(text=f"{str(ctx.author)} - 인증됨\n발송 시간 : {time}",
                          icon_url=ctx.author.avatar_url)
