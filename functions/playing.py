c6c0d9d1a373e8b0dc480b7cb48def260904f349
Whitetiger <teowhite0423@gmail.com>
2021-10-31 14:13:21 +0900

    help 명령어 수정, playing 기능들 모두 슬커로 변경

--- functions/playing.py
+++ functions/playing.py
@@ -9,16 +9,16 @@ class playing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def 가위바위보(self, ctx, user: str):
-        rps_table = ['가위', '바위', '보']
-        if user not in rps_table:
+    @slash_command(description='봇과 가위바위보 게임을 합니다.')
+    async def rsp(self, ctx, user: str):
+        rsp_table = ['가위', '바위', '보']
+        if user not in rsp_table:
             embed = discord.Embed(title="WhiteBot 오류", description="가위바위보 기능", color=0xff0000)
             embed.add_field(name="오류 내용:", value="`가위, 바위, 보` 중에 하나를 입력해주세요.", inline=False)
-            await ctx.send(embed=embed)
+            await ctx.respond(embed=embed)
         else:
-            bot = random.choice(rps_table)
-            result = rps_table.index(user) - rps_table.index(bot)
+            bot = random.choice(rsp_table)
+            result = rsp_table.index(user) - rsp_table.index(bot)
             if result == 0:
                 forsend = (f'{user} vs {bot}\n비겼네요!')
             elif result == 1 or result == -2:
@@ -27,41 +27,41 @@ class playing(commands.Cog):
                 forsend = (f'{user} vs {bot}\n봇이 이겼습니다!')
             embed = discord.Embed(title="가위바위보", description=f"봇 vs {ctx.author.display_name}", color=0xffffff)
             embed.add_field(name="**결과:**", value=f"{forsend}", inline=False)
-            await ctx.send(embed=embed)
+            await ctx.respond(embed=embed)
 
-    @commands.command(aliases=['roll', 'dice'])
-    async def 주사위(self, ctx, 첫번째숫자: int, 두번째숫자: int = None):
+    @slash_command(description='주사위를 굴립니다.')
+    async def dice(self, ctx, 첫번째숫자: int, 두번째숫자: int = None):
         try:
             if 첫번째숫자 < 1:
                 embed = discord.Embed(title="WhiteBot 오류", description="주사위 기능", color=0xff0000)
                 embed.add_field(name="오류 내용:", value="자연수 값만 허용됩니다.", inline=False)
-                await ctx.send(embed=embed)
+                await ctx.respond(embed=embed)
             elif (두번째숫자):
                 embed = discord.Embed(title="주사위", description=f"{첫번째숫자} ~ {두번째숫자}", color=0xffffff)
                 embed.add_field(name="**결과:**", value=f"주사위를 굴렸더니 {random.randint(첫번째숫자, 두번째숫자)}(이)가 나왔어요!", inline=False)
-                await ctx.send(embed=embed)
+                await ctx.respond(embed=embed)
             else:
                 embed = discord.Embed(title="주사위", description=f"1 ~ {첫번째숫자}", color=0xffffff)
                 embed.add_field(name="**결과:**", value=f"주사위를 굴렸더니 {random.randint(1, 첫번째숫자)}(이)가 나왔어요!", inline=False)
-                await ctx.send(embed=embed)
+                await ctx.respond(embed=embed)
         except:
             embed = discord.Embed(title="WhiteBot 오류", description="주사위 기능", color=0xff0000)
             embed.add_field(name="오류 내용:", value="1. 자연수가 아닌 수를 쓰셨는지 확인해주세요.\n2. 첫번째 숫자가 두번째 숫자보다 더 큰지 확인해주세요.\n3. 너무 긴 숫자가 아닌지 확인해주세요.", inline=False)
-            await ctx.send(embed=embed)
+            await ctx.respond(embed=embed)
 
 
-    @commands.command(aliases=['랜덤', 'random', '뭐먹지'])
-    async def 룰렛(self, ctx, *args):
+    @slash_command(description='여러개의 보기 중 하나를 고릅니다.')
+    async def random(self, ctx, *args):
         if args:
             randomlists = list(args)
             choiced = random.choice(randomlists)
             embed = discord.Embed(title="룰렛", description=f'{randomlists}', color=0xffffff)
             embed.add_field(name="**결과:**", value=f'`{choiced}`(이)가 나왔습니다!')
-            await ctx.send(embed=embed)
+            await ctx.respond(embed=embed)
         else:
             embed = discord.Embed(title="WhiteBot 오류", description="랜덤 기능", color=0xff0000)
             embed.add_field(name="오류 내용:", value="올바른 값을 입력해주세요.", inline=False)
-            await ctx.send(embed=embed)
+            await ctx.respond(embed=embed)
 
 def setup(bot):
     bot.add_cog(playing(bot))
