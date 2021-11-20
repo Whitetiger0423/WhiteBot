619a15134bc0e3c27093ebcd657bb529408d47ee
Whitetiger <teowhite0423@gmail.com>
2021-11-20 19:55:24 +0900

    55line에 테스트

--- functions/manage.py
+++ functions/manage.py
@@ -52,6 +52,7 @@ class HelpDropdown(discord.ui.Select):
             min_values=1,
             max_values=1,
             options=options,
+            interaction=discord.Interaction
         )
         
 
