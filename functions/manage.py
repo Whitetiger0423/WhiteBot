34f649d2a689eb41ac83e7701f0b3cc153670905
Whitetiger <teowhite0423@gmail.com>
2021-12-11 18:24:50 +0900

    helptest 커맨드 추가

--- functions/manage.py
+++ functions/manage.py
@@ -121,5 +121,14 @@ class manage(commands.Cog):
         firsthelpembed.add_field(name="봇 초대 링크", value=":link: [봇 초대하기](<https://discord.com/oauth2/authorize?client_id=782777035898617886&permissions=8&scope=bot>)", inline=False)
         await ctx.respond(embed=firsthelpembed, view=view)
 
+    @commands.command(name='helptest')
+    async def helptest(self, ctx):
+        firsthelpembed = discord.Embed(title="<a:check:824251178493411368> WhiteBot 명령어 도움말", description="WhiteBot의 명령어에 대해서 소개합니다.", color=0xffffff)
+        firsthelpembed.add_field(name="/helptest", value="이 메시지를 표출합니다.", inline=False)
+        firsthelpembed.add_field(name="공식 홈페이지", value=":link: [공식 홈페이지](<http://team-white.kro.kr/>)", inline=False)
+        firsthelpembed.add_field(name="공식 서포팅 서버", value=":link: [Team White 공식 서버](<https://discord.gg/aebSVBgzuG>)", inline=False)
+        firsthelpembed.add_field(name="봇 초대 링크", value=":link: [봇 초대하기](<https://discord.com/oauth2/authorize?client_id=782777035898617886&permissions=8&scope=bot>)", inline=False)
+        await ctx.send(embed=firsthelpembed)
+
 def setup(bot):
     bot.add_cog(manage(bot))
