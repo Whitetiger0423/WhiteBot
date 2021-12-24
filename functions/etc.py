2dc822355949ca6a80e8a6f1bdaef79aed950f3f
Whitetiger <teowhite0423@gmail.com>
2021-12-24 17:48:26 +0900

    암호/해독 명령어 오타 및 통일성 수정

--- functions/etc.py
+++ functions/etc.py
@@ -48,20 +48,20 @@ class etc(commands.Cog):
             string_bytes = text.encode("utf-8")
             base64_bytes = base64.b64encode(string_bytes) 
             data = base64_bytes.decode("utf-8") 
-            embed = discord.Embed(title="<a:check:824251178493411368> 해독 완료!", description="base64를 기반으로 한 암호문입니다.", color=0xffffff)
-            embed.add_field(name="**해독 결과:**", value=f"```{data}```", inline=False)
+            embed = discord.Embed(title="<a:check:824251178493411368> 암호화 완료!", description="base64를 기반으로 한 암호문입니다.", color=0xffffff)
+            embed.add_field(name="**암호문:**", value=f"```{data}```", inline=False)
             await ctx.respond(embed=embed)
 
     @slash_command(description='수신문을 해독합니다.')
-    async def decode(self, ctx, type: Option(str, "해독할 암호몬의 암호화 방식을 선택하세요", choices=["아스키 코드", "base64"]), text):
+    async def decode(self, ctx, type: Option(str, "해독할 암호문의 암호화 방식을 선택하세요", choices=["아스키 코드", "base64"]), text):
         if type == "아스키 코드":
             data = decrypt(text)
             try:
                 embed = discord.Embed(title="<a:check:824251178493411368> 해독 완료!", description="아스키 코드를 기반으로 한 암호문을 해독하였습니다.\n해독이 잘못되었다면 [서포팅 서버](<https://discord.gg/aebSVBgzuG>)에서 제보해주세요!", color=0xffffff)
-                embed.add_field(name="**해독 결과:**", value=f"```{data}```", inline=False)
+                embed.add_field(name="**해독문:**", value=f"```{data}```", inline=False)
                 await ctx.respond(embed=embed)
             except:
-                embed = discord.Embed(title="WhiteBot 오류", description="복호화 기능", color=0xff0000)
+                embed = discord.Embed(title="WhiteBot 오류", description="해독 기능", color=0xff0000)
                 embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 올바른 암호문을 입력해주세요.", inline=False)
                 await ctx.respond(embed=embed)
         elif type == "base64":
@@ -73,7 +73,7 @@ class etc(commands.Cog):
                 embed.add_field(name="**해독 결과:**", value=f"```{data}```", inline=False)
                 await ctx.respond(embed=embed)
             except:
-                embed = discord.Embed(title="WhiteBot 오류", description="복호화 기능", color=0xff0000)
+                embed = discord.Embed(title="WhiteBot 오류", description="해독 기능", color=0xff0000)
                 embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 올바른 암호문을 입력해주세요.", inline=False)
                 await ctx.respond(embed=embed)
 
