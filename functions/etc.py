bb453cc65e0a037bafbf767581edf8d2cd043005
Whitetiger <teowhite0423@gmail.com>
2021-12-25 14:57:55 +0900

    base85 추가, 오류 메시지 수정

--- functions/etc.py
+++ functions/etc.py
@@ -38,7 +38,7 @@ class etc(commands.Cog):
         self.bot = bot
 
     @slash_command(description='수신문을 암호화합니다.')
-    async def code(self, ctx, type: Option(str, "암호화 시킬 방식을 선택하세요", choices=["base16", "base32", "base64", "아스키 코드"]), text):
+    async def code(self, ctx, type: Option(str, "암호화 시킬 방식을 선택하세요", choices=["base16", "base32", "base64", "base85" "아스키 코드"]), text):
         if type == "base16":
             string_bytes = text.encode("utf-8")
             base16_bytes = base64.b16encode(string_bytes) 
@@ -60,6 +60,13 @@ class etc(commands.Cog):
             embed = discord.Embed(title="<a:check:824251178493411368> 암호화 완료!", description="**base64**를 기반으로 한 암호문입니다.", color=0xffffff)
             embed.add_field(name="**암호문:**", value=f"```{data}```", inline=False)
             await ctx.respond(embed=embed)
+        elif type == "base85":
+            string_bytes = text.encode("utf-8")
+            base85_bytes = base64.b85encode(string_bytes) 
+            data = base85_bytes.decode("utf-8") 
+            embed = discord.Embed(title="<a:check:824251178493411368> 암호화 완료!", description="**base85**를 기반으로 한 암호문입니다.", color=0xffffff)
+            embed.add_field(name="**암호문:**", value=f"```{data}```", inline=False)
+            await ctx.respond(embed=embed)
         elif type == "아스키 코드":
             data = encrypt(text)
             embed = discord.Embed(title="<a:check:824251178493411368> 암호화 완료!", description="**아스키 코드**를 기반으로 한 암호문입니다.", color=0xffffff)
@@ -67,7 +74,7 @@ class etc(commands.Cog):
             await ctx.respond(embed=embed)
 
     @slash_command(description='수신문을 해독합니다.')
-    async def decode(self, ctx, type: Option(str, "해독할 암호문의 암호화 방식을 선택하세요", choices=["base16", "base32", "base64", "아스키 코드"]), text):
+    async def decode(self, ctx, type: Option(str, "해독할 암호문의 암호화 방식을 선택하세요", choices=["base16", "base32", "base64", "base85", "아스키 코드"]), text):
         if type == "base16":
             try:
                 string_bytes = text.encode("utf-8")
@@ -78,7 +85,7 @@ class etc(commands.Cog):
                 await ctx.respond(embed=embed)
             except:
                 embed = discord.Embed(title="WhiteBot 오류", description="해독 기능", color=0xff0000)
-                embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 올바른 암호문을 입력해주세요.", inline=False)
+                embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 암호의 종류가 맞는지 확인해주시고, 올바른 암호문을 입력해주세요.", inline=False)
                 await ctx.respond(embed=embed)
         elif type == "base32":
             try:
@@ -90,7 +97,7 @@ class etc(commands.Cog):
                 await ctx.respond(embed=embed)
             except:
                 embed = discord.Embed(title="WhiteBot 오류", description="해독 기능", color=0xff0000)
-                embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 올바른 암호문을 입력해주세요.", inline=False)
+                embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 암호의 종류가 맞는지 확인해주시고, 올바른 암호문을 입력해주세요.", inline=False)
                 await ctx.respond(embed=embed)
         elif type == "base64":
             try:
@@ -102,7 +109,19 @@ class etc(commands.Cog):
                 await ctx.respond(embed=embed)
             except:
                 embed = discord.Embed(title="WhiteBot 오류", description="해독 기능", color=0xff0000)
-                embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 올바른 암호문을 입력해주세요.", inline=False)
+                embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 암호의 종류가 맞는지 확인해주시고, 올바른 암호문을 입력해주세요.", inline=False)
+                await ctx.respond(embed=embed)
+        elif type == "base85":
+            try:
+                string_bytes = text.encode("utf-8")
+                base85_bytes = base64.b85decode(string_bytes) 
+                data = base85_bytes.decode("utf-8")
+                embed = discord.Embed(title="<a:check:824251178493411368> 해독 완료!", description="**base85**를 기반으로 한 암호문을 해독하였습니다.\n해독이 잘못되었다면 [서포팅 서버](<https://discord.gg/aebSVBgzuG>)에서 제보해주세요!", color=0xffffff)
+                embed.add_field(name="**해독 결과:**", value=f"```{data}```", inline=False)
+                await ctx.respond(embed=embed)
+            except:
+                embed = discord.Embed(title="WhiteBot 오류", description="해독 기능", color=0xff0000)
+                embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 암호의 종류가 맞는지 확인해주시고, 올바른 암호문을 입력해주세요.", inline=False)
                 await ctx.respond(embed=embed)
         elif type == "아스키 코드":
             data = decrypt(text)
@@ -112,7 +131,7 @@ class etc(commands.Cog):
                 await ctx.respond(embed=embed)
             except:
                 embed = discord.Embed(title="WhiteBot 오류", description="해독 기능", color=0xff0000)
-                embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 올바른 암호문을 입력해주세요.", inline=False)
+                embed.add_field(name="오류 내용:", value="올바르지 않은 암호문입니다. 암호의 종류가 맞는지 확인해주시고, 올바른 암호문을 입력해주세요.", inline=False)
                 await ctx.respond(embed=embed)
 
     @slash_command(description='검색어를 검색합니다.')
