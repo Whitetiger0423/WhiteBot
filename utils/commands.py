7335fde68cea1c82d263f0c27acbca207e3116d1
nkgcp <nkgcp05@gmail.com>
2022-01-08 19:24:07 +0900

    재귀 오버 오류 제거
    `slash_command` 함수 내에서 호출하는 `slash_command`가 `discord.commands` 패키지 내의 함수가 아니라 자기 자신을 가리켜 발생한 재귀 오류를 해결했습니다.

--- utils/commands.py
+++ utils/commands.py
@@ -1,5 +1,5 @@
 import os
-from discord.commands import slash_command
+from discord.commands import SlashCommand, application_command
 from utils.utils import apply_if_not_none
 
 TEST_GUILD_ID = apply_if_not_none(os.getenv("TEST_GUILD_ID"), lambda x : int(x))
@@ -8,4 +8,4 @@ def slash_command(**kwargs):
     if TEST_GUILD_ID is not None:
         kwargs['guild_ids'] = [TEST_GUILD_ID]
 
-    return slash_command(**kwargs)
+    return application_command(cls=SlashCommand, **kwargs)
