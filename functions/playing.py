8496902a9e827f9b8764819807e9856d58c3d4f4
Whitetiger <teowhite0423@gmail.com>
2022-01-20 15:27:18 +0900

    홀짝 명령어 playing.py로 이동

--- functions/playing.py
+++ functions/playing.py
@@ -83,6 +83,39 @@ class playing(commands.Cog):
                 inline=False,
             )
             await ctx.respond(embed=embed)
+    @slash_command(description="홀짝 게임을 합니다.")
+    async def 홀짝(ctx):
+    import random
+        dice = random.randint(1, 6)
+        embed = discord.Embed(title='홀, 짝중에 하나를 선택해주세요.',
+                          description='선택 한 뒤에 어떤 수가 나왔는지 알려드려요.')
+        embed.add_field(name='> 주사위의 눈', value='???')
+        embed.add_field(name='> 홀수', value='🔴')
+        embed.add_field(name='> 짝수', value='🔵')
+        msg = await ctx.respond(embed=embed)
+        await msg.add_reaction('🔴')
+        await msg.add_reaction('🔵')
+        try:
+
+            def check(reaction, user):
+                return str(reaction) in ['🔴', '🔵'] and \
+                user == ctx.author and reaction.message.id == msg.id
+
+            reaction, user = await bot.wait_for('reaction_add', check=check)
+            if  (str(reaction) == '🔴' and dice % 2 == 1) or \
+                (str(reaction) == '🔵' and dice % 2 == 0):
+                embed = discord.Embed(title='홀, 짝중에 하나를 선택해주세요.',
+                                  description='정답입니다! 계속해서 도전해보세요!')
+            else:
+                embed = discord.Embed(title='홀, 짝중에 하나를 선택해주세요.',
+                                  description='틀렸습니다... 계속해서 도전해보세요!')
+            embed.add_field(name='> 주사위의 눈', value=str(dice))
+            embed.add_field(name='> 홀수', value='🔴')
+            embed.add_field(name='> 짝수', value='🔵')
+            await msg.clear_reactions()
+            await msg.edit(embed=embed)
+        except:
+            pass
 
 
 def setup(bot):
