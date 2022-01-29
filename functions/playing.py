cff84d82e724843bcaf01fe334c2ab1c7a41ac2c
Whitetiger <teowhite0423@gmail.com>
2022-01-29 18:31:11 +0900

    틱택토 초본 추가 - 정상 작동은 하나 개선 필요

--- functions/playing.py
+++ functions/playing.py
@@ -3,6 +3,7 @@ import random
 from discord.ext import commands
 from utils.commands import slash_command
 from discord.commands import ApplicationContext, Option
+from typing import List
 
 
 class playing(commands.Cog):
@@ -84,6 +85,107 @@ class playing(commands.Cog):
                 inline=False,
             )
             await ctx.respond(embed=embed)
+
+    @slash_command(description="틱택토(삼목) 게임을 진행합니다.")
+    async def tictactoe(self, ctx):
+        await ctx.respond("틱택토(삼목) 게임을 시작합니다. X부터 시작해요!", view=TicTacToe())
+
+
+class TicTacToeButton(discord.ui.Button["TicTacToe"]):
+    def __init__(self, x: int, y: int):
+        super().__init__(style=discord.ButtonStyle.secondary, label="\u200b", row=y)
+        self.x = x
+        self.y = y
+
+    async def callback(self, interaction: discord.Interaction):
+        assert self.view is not None
+        view: TicTacToe = self.view
+        state = view.board[self.y][self.x]
+        if state in (view.X, view.O):
+            return
+
+        if view.current_player == view.X:
+            self.style = discord.ButtonStyle.danger
+            self.label = "X"
+            self.disabled = True
+            view.board[self.y][self.x] = view.X
+            view.current_player = view.O
+            content = "O의 차례입니다!"
+        else:
+            self.style = discord.ButtonStyle.success
+            self.label = "O"
+            self.disabled = True
+            view.board[self.y][self.x] = view.O
+            view.current_player = view.X
+            content = "X의 차례입니다!"
+
+        winner = view.check_board_winner()
+        if winner is not None:
+            if winner == view.X:
+                content = "X 승리!"
+            elif winner == view.O:
+                content = "O 승리!"
+            else:
+                content = "비겼습니다."
+
+            for child in view.children:
+                child.disabled = True
+
+            view.stop()
+
+        await interaction.response.edit_message(content=content, view=view)
+
+
+class TicTacToe(discord.ui.View):
+    children: List[TicTacToeButton]
+    X = -1
+    O = 1
+    Tie = 2
+
+    def __init__(self):
+        super().__init__()
+        self.current_player = self.X
+        self.board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+
+        for x in range(3):
+            for y in range(3):
+                self.add_item(TicTacToeButton(x, y))
+
+    def check_board_winner(self):
+        for across in self.board:
+            value = sum(across)
+            if value == 3:
+                return self.O
+            elif value == -3:
+                return self.X
+
+        for line in range(3):
+            value = self.board[0][line] + self.board[1][line] + self.board[2][line]
+            if value == 3:
+                return self.O
+            elif value == -3:
+                return self.X
+
+        diag = self.board[0][2] + self.board[1][1] + self.board[2][0]
+        if diag == 3:
+            return self.O
+        elif diag == -3:
+            return self.X
+
+        diag = self.board[0][0] + self.board[1][1] + self.board[2][2]
+        if diag == 3:
+            return self.O
+        elif diag == -3:
+            return self.X
+
+        if all(i != 0 for row in self.board for i in row):
+            return self.Tie
+
+        return None
 
 
 def setup(bot):
