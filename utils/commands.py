48f72c421fc0844cfd98b338237366a071cab5cb
nkgcp <nkgcp05@gmail.com>
2022-01-29 20:48:59 +0900

    ErrorHandlingSlashCommand 구현
    기존 SlashCommand 클래스를 상속하여, 예외가 발생할 경우 알아서
    오류 발생 메시지를 보내주는 클래스를 구현하였습니다.
    또한 WhiteBotException을 만들어, 이 예외가 발생할 경우 정해둔 내용으로
    임베드를 보내주는 기능이 있습니다. 다만, 아직까지는 기존 코드에 적용되지
    않아 잡아내지 못한 오류를 처리하는데만 쓰일 것으로 보입니다.
    추후에 제대로 적용하면 일관성 있는 오류 메시지를 보내는데 유용할
    것입니다.

--- utils/commands.py
+++ utils/commands.py
@@ -1,12 +1,46 @@
+import logging
 import os
-from discord.commands import SlashCommand, application_command
+import discord
+from discord.commands import SlashCommand, ApplicationContext, application_command
 from utils.utils import apply_if_not_none
+from typing import Callable, Optional
 
 TEST_GUILD_ID = apply_if_not_none(os.getenv("TEST_GUILD_ID"), lambda x: int(x))
 
+logger = logging.getLogger(__name__)
+
+class ErrorHandlingSlashCommand(SlashCommand):
+    def __init__(self, func: Callable, *args, **kwargs) -> None:
+        super().__init__(func, *args, **kwargs)
+        logger.info(f"{self.name} command registered")
+
+        self._callback = func
+        self.callback = self.error_handler
+
+    def error_handler(self, ctx: ApplicationContext, **kwargs):
+        try:
+            self._callback(ctx, kwargs)
+        except WhiteBotException as e:
+            ctx.respond(embed=discord.Embed(
+                title=e.title or "오류가 발생했어요",
+                description=e.description,
+                color=0xFF0000
+            ))
+        except Exception as e:
+            logger.exception(f"Exception from {self.name}")
+            ctx.respond(embed=discord.Embed(
+                title="알 수 없는 오류가 발생했어요",
+                description="잠시 후에 다시 시도해주세요",
+                color=0xFF0000
+            ))
+
+class WhiteBotException(RuntimeError):
+    def __init__(self, description: str, title: Optional[str] = None):
+        self.description = description
+        self.title = title
 
 def slash_command(**kwargs):
     if TEST_GUILD_ID is not None:
         kwargs["guild_ids"] = [TEST_GUILD_ID]
 
-    return application_command(cls=SlashCommand, **kwargs)
+    return application_command(cls=ErrorHandlingSlashCommand, **kwargs)
