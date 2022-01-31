8bda31bc77eb781eaf7a69ddb9cf7308aa31916a
nkgcp <nkgcp05@gmail.com>
2022-01-31 21:25:31 +0900

    투표 기능 코드 재작성
    변경 사항은 아래와 같습니다.
    
    * 서비스 클래스를 제거하고 각 명령어 함수에서 쿼리문을 직접 실행합니다.
    * end_vote 명령어에서 vote_id 파라미터를 필수로 받습니다.
    * end_vote 명령어의 vote_id 파라미터 타입을 정수로 변경했습니다.
    * vote_choices 테이블에 있던 불필요한 value 칼럼을 삭제했습니다.
    * 코드를 포맷팅했으며, 쿼리 결과를 디컨스트럭팅으로 받는 등 코드를 보기
      좋게 수정하였습니다.
    * 명령어 옵션의 디스크립션을 수정하였습니다.
    
    이로 인해 얻을 수 있는 효과는 다음과 같습니다.
    
    * 코드의 직관성이 좋아져 버그 해결이 용이해집니다.
    * 커서 생성, 쿼리 실행, 커서 삭제, 커밋의 이상적인 사이클을 구성할 수
      있습니다.
    * 쿼리문 파라미터로 들어가는 데이터의 타입이 문자열이 아니라서 발생하는
      타입 오류를 해결했습니다.
    
    추후 구현해야할 기능은 다음과 같습니다.
    
    * 투표 익명 여부 선택 기능
    * 실시간 선택 상황 공개 기능
    * 중복 투표 가능 여부 선택 기능
    * 투표 임베드 내용 커스텀 기능
    * 투표 버튼 이모지와 색깔 커스텀 기능
    * 오류 처리 및 데이터베이스 변경 사항 롤백
    
    로그문들이 추후 재작성을 위해 제거되었습니다.

--- functions/vote.py
+++ functions/vote.py
@@ -5,142 +5,94 @@ from utils.commands import slash_command
 from discord.ui import View, Button
 import sqlite3
 import os
-import re
 import logging
 
 logger = logging.getLogger(__name__)
 
-class vote(commands.Cog):
-    VOTE_ID_REGEX = re.compile("#?(\\d+)")
 
+class vote(commands.Cog):
     def __init__(self):
         db_path = os.getenv("DATABASE_PATH") or ":memory:"
-        self.service = vote_service(db_path)
-        self.service.create_tables()
+        self.conn = sqlite3.connect(db_path)
+
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS votes(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, state INTEGER, user_id INTEGER, interaction_token TEXT)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS vote_choices(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, vote REFERENCES votes)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS voters(id INTEGER, choice REFERENCES voice_choices)")
+        cursor.close()
 
     @slash_command(description="투표를 시작합니다")
     async def vote(
         self,
         ctx: ApplicationContext,
-        name: Option(str, description="투표 이름"),
-        choices: Option(str, description="선택지(쉼표로 구분, 최대 25개)")
+        name: Option(str, description="새로 만들 투표의 이름입니다"),
+        choices: Option(str, description="투표의 선택지입니다(쉼표로 구분, 최대 25개)")
     ):
         await ctx.defer()
 
-        vote_id = self.service.create_vote(name, ctx.author.id, ctx.interaction.token)
-        logger.info(f"vote entity created(id: {vote_id}, name: {name})")
+        cursor = self.conn.cursor()
+
+        cursor.execute(f"INSERT INTO votes (name, state, user_id, interaction_token) VALUES ('{name}', 0, {ctx.author.id}, '{ctx.interaction.token}')")
+        vote_id = cursor.lastrowid
 
         choices = choices.split(",", 25)
         view = View()
         for choice in choices:
             choice_name = choice.strip()
-            choice_id = self.service.create_vote_choice(choice_name, vote_id)
-            logger.info(f"vote choice entity created(id: {choice_id}, name: {choice_name}, vote: {vote_id})")
+            cursor.execute(f"INSERT INTO vote_choices (name, vote) VALUES ('{choice_name}', {vote_id})")
+            choice_id = cursor.lastrowid
 
-            button = Button(style = discord.ButtonStyle.primary, label = choice_name, custom_id = str(choice_id))
+            button = Button(style=discord.ButtonStyle.primary, label=choice_name, custom_id=str(choice_id))
             button.callback = self.button_callback
             view.add_item(button)
 
         await ctx.respond(
             embed=discord.Embed(title=f"#{vote_id} {name}", description="아래 버튼을 눌러 투표에 참여해주세요.", color=0xFFFFFF)
-                .set_footer(text=f"Started by {ctx.author.display_name}", icon_url=ctx.author.display_avatar),
+            .set_footer(text=f"Started by {ctx.author.display_name}", icon_url=ctx.author.display_avatar),
             view=view
         )
+        cursor.close()
+        self.conn.commit()
 
     async def button_callback(self, interaction: discord.Interaction):
+        cursor = self.conn.cursor()
+
         user_id = interaction.user.id
         choice_id = interaction.data["custom_id"]
-        vote_id = self.service.get_choice(choice_id)[3]
 
-        if self.service.get_vote(vote_id)[2] != 0:
+        cursor.execute(f"SELECT vote FROM vote_choices WHERE id={choice_id}")
+        (vote_id,) = cursor.fetchone()
+
+        cursor.execute(f"SELECT state FROM votes WHERE id={vote_id}")
+        (vote_state,) = cursor.fetchone()
+        if vote_state != 0:
             return await interaction.response.send_message("투표가 종료되었습니다", ephemeral=True)
 
-        self.service.create_voter(user_id, choice_id)
-        logger.info(f"voter entity created(id: {user_id}, choice: {choice_id})")
+        cursor.execute(f"INSERT INTO voters (id, choice) VALUES ({user_id}, {choice_id})")
 
-    @slash_command(description="투표를 끝냅니다")
-    async def end_vote(self, ctx: ApplicationContext, _vote_id: Option(str, description="투표 아이디(미입력시 가장 최근에 시작한 투표)", required = False)):
+        cursor.close()
+        self.conn.commit()
+
+    @slash_command(description="투표를 종료합니다")
+    async def end_vote(self, ctx: ApplicationContext, vote_id: Option(int, description="종료할 투표 아이디입니다")):
         await ctx.defer()
 
-        if _vote_id is None:
-            vote_id = self.service.get_latest_vote(ctx.user.id)
-        else:
-            regex_match = self.VOTE_ID_REGEX.match(_vote_id)
-            if regex_match is None:
-                return await ctx.respond("투표 아이디가 올바르지 않습니다", ephemeral=True)
-            vote_id = regex_match.group(1)
-
-        self.service.set_vote_state(1, vote_id)
-
-        vote = self.service.get_vote(vote_id)
-        embed = discord.Embed(title="투표 결과", description=f"#{vote_id} {vote[1]}\n투표가 종료되었습니다", color=0xFFFFFF)
-
-        for choice in self.service.get_choices(vote_id):
-            embed.add_field(name=choice[1], value=self.service.get_voter_count(choice[0]))
-
-        ctx.respond(embed=embed)
-
-
-class vote_service():
-    CREATE_TABLES = [
-        "CREATE TABLE IF NOT EXISTS votes(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, state INTEGER, user_id INTEGER, interaction_token TEXT)",
-        "CREATE TABLE IF NOT EXISTS vote_choices(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, value INTEGER, vote REFERENCES votes)",
-        "CREATE TABLE IF NOT EXISTS voters(id INTEGER PRIMARY KEY, choice REFERENCES voice_choices)"
-    ]
-    CREATE_VOTE = "INSERT INTO votes (name, state, user_id, interaction_token) VALUES (?, 0, ?, ?)"
-    CREATE_VOTE_CHOICE = "INSERT INTO vote_choices (name, value, vote) VALUES (?, 0, ?)"
-    CREATE_VOTER = "INSERT INTO voters (id, choice) VALUES (?, ?)"
-
-    GET_LATEST_VOTE = "SELECT * FROM votes WHERE user_id=? ORDER BY id DESC LIMIT 1"
-    GET_VOTE = "SELECT * FROM votes WHERE id=?"
-    GET_CHOICE = "SELECT * FROM choices WHERE id=?"
-    GET_CHOICES = "SELECT * FROM choices WHERE vote=?"
-    GET_VOTER_COUNT = "SELECT * FROM voters WHERE choice=?"
-
-    SET_VOTE_STATE = "UPDATE votes SET state=? WHERE id=?"
-
-    def __init__(self, db_path: str):
-        self.conn = sqlite3.connect(db_path)
-
-    def create_tables(self):
         cursor = self.conn.cursor()
-        for smt in self.CREATE_TABLES:
-            cursor.execute(smt)
+        cursor.execute(f"UPDATE votes SET state=1 WHERE id={vote_id}")
 
-    def create_vote(self, name, user_id, interaction_token) -> int:
-        cursor = self.conn.execute(self.CREATE_VOTE, (name, user_id, interaction_token))
-        return cursor.lastrowid
+        cursor.execute(f"SELECT name FROM votes WHERE id={vote_id}")
+        (vote_name,) = cursor.fetchone()
+        embed = discord.Embed(title=f"#{vote_id} {vote_name}", description="투표가 종료되었습니다", color=0xFFFFFF)
 
-    def create_vote_choice(self, name, vote) -> int:
-        cursor = self.conn.execute(self.CREATE_VOTE_CHOICE, (name, vote))
-        return cursor.lastrowid
+        cursor.execute(f"SELECT id, name FROM vote_choices WHERE vote={vote_id}")
+        for (choice_id, choice_name) in cursor.fetchall():
+            cursor.execute(f"SELECT * FROM voters WHERE choice={choice_id}")
+            voter_count = len(cursor.fetchall())
+            embed.add_field(name=choice_name, value=voter_count)
 
-    def create_voter(self, id, choice) -> int:
-        cursor = self.conn.execute(self.CREATE_VOTER, (id, choice))
-        return cursor.lastrowid
-
-    def get_latest_vote(self, user_id) -> int:
-        cursor = self.conn.execute(self.GET_LATEST_VOTE, (user_id))
-        return cursor.lastrowid
-
-    def get_vote(self, vote_id) -> tuple:
-        cursor = self.conn.execute(self.GET_VOTE, (vote_id))
-        return cursor.fetchone()
-
-    def get_choice(self, choice_id) -> tuple:
-        cursor = self.conn.execute(self.GET_CHOICE, (choice_id))
-        return cursor.fetchone()
-
-    def get_choices(self, vote_id) -> list[tuple]:
-        cursor = self.conn.execute(self.GET_CHOICES, (vote_id))
-        return cursor.fetchall()
-
-    def get_voter_count(self, choice_id) -> int:
-        cursor = self.conn.execute(self.GET_VOTER_COUNT, (choice_id))
-        return cursor.rowcount
-
-    def set_vote_state(self, state, vote_id) -> None:
-        self.conn.execute(self.SET_VOTE_STATE, (state, vote_id))
+        await ctx.respond(embed=embed)
+        cursor.close()
+        self.conn.commit()
 
 
 def setup(bot: discord.Bot):
