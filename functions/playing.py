a2d2ce1e1bc069698d0c754d4e65d12eb615840c
star0202 <61998307+star0202@users.noreply.github.com>
2022-02-18 16:29:51 +0900

    틱택토 기능에서 봇과 대결할 수 있던 오류 수정

--- functions/playing.py
+++ functions/playing.py
@@ -88,7 +88,13 @@ class playing(commands.Cog):
 
     @slash_command(description="틱택토(삼목) 게임을 진행합니다.")
     async def tictactoe(self, ctx: ApplicationContext, rival: Option(discord.User, description="같이 게임을 할 유저를 선택하세요")):
-        await ctx.respond("틱택토(삼목) 게임을 시작합니다. X부터 시작해요!", view=TicTacToe(ctx.user.id, rival.id))
+        if rival.bot:
+            embed = discord.Embed(
+                    title="WhiteBot 오류", description="틱택토 기능", color=0xFF0000
+                )
+            embed.add_field(name="오류 내용:", value="봇과는 대결할 수 없습니다.", inline=False)
+            await ctx.respond(embed=embed)
+        else:await ctx.respond("틱택토(삼목) 게임을 시작합니다. X부터 시작해요!", view=TicTacToe(ctx.user.id, rival.id))
 
 
 class TicTacToeButton(discord.ui.Button["TicTacToe"]):
