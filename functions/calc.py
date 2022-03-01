9f69b777647c18487e223d9951f2b5e8a6c97b48
KeySpace <77488170+froggal@users.noreply.github.com>
2022-03-01 15:28:20 +0900

    upload calc.py file
    계산기 만들었고 오류 잇으면 수정 부탁드립니다~

---
+++ functions/calc.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2022 Team White
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import discord
+from discord.ext import commands
+from discord.commands import ApplicationContext, Option
+from utils.commands import slash_command
+
+
+class calc(commands.Cog):
+    @slash_command(description="간단한 연산을 수행합니다.")
+    async def calc(
+        self,
+        ctx: ApplicationContext,
+        type: Option(
+            str,
+            "할 연산을 선택하세요.",
+            choices=["더하기", "빼기", "나누기", "곱하기"],
+        ),
+        first: Option(str, "연산할 첫번째수를 입력하세요"),
+        second: Option(str, "연산할 첫번째수를 입력하세요"),
+    ):
+        if type == "더하기":
+            equal = first + second
+        elif type == "빼기":
+            equal = first - second
+        elif type == "곱하기":
+            equal = first * second
+        elif type == "나누기":
+            equal = first / second ,', ', first % second
+        embed = discord.Embed(
+            title="<a:check:824251178493411368> 계산 완료!",
+            description=f"**{type}**연산의 결과입니다.",
+            color=0xFFFFFF,
+        ).add_field(name="**결과:**", value=f"```{equal}```", inline=False)
+
+        await ctx.respond(embed=embed)
+
+def setup(bot):
+    bot.add_cog(calc())
