c22ab3ddf6bf97533487e9e0b05366d85cc1a084
HydrOxy2gen <100982305+HydrOxy2gen@users.noreply.github.com>
2022-05-23 00:16:04 +0900

    변수명 한글화, embed 문구 자연스럽게 수정

--- functions/advanced_caculator.py
+++ functions/advanced_caculator.py
@@ -15,40 +15,40 @@ class calcadvance(commands.Cog):
             str,
             "값을 선택할 삼각비 또는 삼각함수의 종류를 선택하세요.",
             choices=[
-                "Cosine",
-                "Sine",
-                "Tansent",
-                "Cosequent",
-                "Sequent",
-                "Cotangent",
-                "Factorial",
-                "Log",
+                "코사인",
+                "사인",
+                "탄젠트",
+                "코시컨트",
+                "시컨트",
+                "코탄젠트",
+                "팩토리얼",
+                "로그",
             ],
         ),
         first: Option(int, "값을 입력해주세요"),
     ):
-        if type == "Cosine":
+        if type == "코사인":
             equal = str(math.cos(math.pi * (first / 180)))
-        elif type == "Sine":
+        elif type == "사인":
             equal = str(math.sin(math.pi * (first / 180)))
-        elif type == "Tansent":
+        elif type == "탄젠트":
             equal = str(math.tan(math.pi * (first / 180)))
-        elif type == "Cosequent":
+        elif type == "코시컨트":
             equal = str(1.0 / math.cos(first))
-        elif type == "Sequent":
+        elif type == "시컨트":
             equal = str(1.0 / math.sin(first))
-        elif type == "Cotangent":
+        elif type == "코탄젠트":
             equal = str(1.0 / math.tan(first))
             str(1.0 / math.sin(txt))
-        elif type == "Factorial":
+        elif type == "팩토리얼":
             equal = str(math.factorial(first))
-        elif type == "Log":
+        elif type == "로그":
             equal = str(math.log(first))
         embed = discord.Embed(
             title="<a:check:824251178493411368> 계산 완료!",
-            description=f"{type} {first}의 결과입니다.",
+            description=f"{type} {first}의 계산 결과입니다.",
             color=0xFFFFFF,
-        ).add_field(name="**결과:**", value=f"```{equal}```", inline=False)
+        ).add_field(name="**계산 결과:**", value=f"```{equal}```", inline=False)
 
         await ctx.respond(embed=embed)
 
