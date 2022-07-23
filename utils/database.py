3e1a37fac60bd4a75f48086fc2c615bded95f153
9Hundred100Five1 <69020075+9Hundred100Five1@users.noreply.github.com>
2022-07-23 21:07:08 +0900

    오타 수정
    mongodb collection의 이름을 WhiteBot으로 수정했습니다.

--- utils/database.py
+++ utils/database.py
@@ -7,7 +7,7 @@ dotenv.load_dotenv("./token.env", verbose=True)
 MONGO_URI = os.getenv("MONGO_URI")
 
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI).HaRimBa
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI).WhiteBot
 
 
 class CURRENCY_DATABASE:
