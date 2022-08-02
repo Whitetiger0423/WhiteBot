30887fedddc43cf6624db70271860d8fcd80d375
Starcea <stardev.uwu@gmail.com>
2022-08-02 14:14:43 +0900

    feat: WhiteBot class

--- main.py
+++ main.py
@@ -13,48 +13,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-import discord
-import os
-import dotenv
-import logging
-import utils.logging
-import utils.koreanbots
-from discord.ext import commands, tasks
-import time
+from utils.whitebot import WhiteBot
+from dotenv import load_dotenv
 
-dotenv.load_dotenv()
-utils.logging.setup_logging()
+load_dotenv(".env")
 
-bot = commands.Bot(command_prefix="/", help_command=None)
-aiodb = None
-logger = logging.getLogger("main")
-
-bot.start_time = time.time()
-
-
-@bot.event
-async def on_ready():
-    guild_count = len(bot.guilds)
-    logger.info(f"Logged in as {bot.user.name}")
-    logger.info(f"Be used in {guild_count} guilds.")
-
-    dbkr_token = os.getenv("DBKR_TOKEN")
-    await utils.koreanbots.update_guild_count(dbkr_token, bot.user.id, guild_count)
-
-    change_status.start()
-
-
-@tasks.loop(seconds=5)  # n초마다 다음 메시지 출력
-async def change_status():
-    await bot.change_presence(
-        status=discord.Status.online,
-        activity=discord.Game(f"버전 1.10.0 - {len(bot.guilds)}개의 서버에서 작동 중"),
-    )
-
-
-for filename in os.listdir("functions"):
-    if filename.endswith(".py"):
-        bot.load_extension(f"functions.{filename[:-3]}")
-
-
-bot.run(os.getenv("BOT_TOKEN"))
+if __name__ == "__main__":
+    bot = WhiteBot()
+    bot.run()
