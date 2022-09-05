8a264efefd12494304a5c151afe91c0875d04694
Starcea <stardev.uwu@gmail.com>
2022-09-06 08:11:38 +0900

    fix: unused module

--- utils/whitebot.py
+++ utils/whitebot.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from ast import Constant
 from discord.ext import commands, tasks
 import discord
 from utils.logging import setup_logging
